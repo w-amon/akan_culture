d929a55c4713ac9e9879bcc5ca16ff50b5eadcdc
Amon W <amon@Amons-MacBook-Air.local>
2023-11-10 14:40:16 +0300

    Add fucntionality

--- js/script.js
+++ js/script.js
@@ -33,3 +33,18 @@ function invalid(day, month) {
         return true;
     }
 }
+
+
+// Alternative for call function - returns Boolean
+var CC, YY, MM, DD, d, dayValue;
+
+ function calculateDayValue() {
+     year = document.getElementById("year").value;
+     CC = parseInt(year.substring(0, 2));
+     YY = parseInt(year.substring(2, 4));
+     MM = parseInt(document.getElementById("month").value);
+     DD = parseInt(document.getElementById("date").value);
+     d = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
+     console.log(d);
+     return (Math.round(d));
+ }
